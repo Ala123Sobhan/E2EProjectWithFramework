a54f62e18d415ed9c4ca2747d8131940490210ba
Ala123Sobhan <ala123sobhan@gmail.com>
2021-07-11 10:01:06 -0400

    adding dataProvider to run testcases with different data

--- src/test/java/Academy/HomePage.java
+++ src/test/java/Academy/HomePage.java
@@ -2,6 +2,7 @@ package Academy;
 
 import java.io.IOException;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import pageObjects.LandingPage;
@@ -9,8 +10,8 @@ import pageObjects.LoginPage;
 import resources.base;
 
 public class HomePage extends base {
-	@Test
-	public void verifyPageNavigation() throws IOException {
+	@Test(dataProvider = "getData")
+	public void verifyPageNavigation(String uname, String pass, String status) throws IOException {
 		driver = initializeDriver();
 		driver.get("http://www.qaclickacademy.com/");
 		
@@ -18,9 +19,24 @@ public class HomePage extends base {
 		l.getsignIn().click();
 		
 		LoginPage lp = new LoginPage(driver);
-		lp.getEmail().sendKeys("bob@ht.com");
-		lp.getPass().sendKeys("1234!");
+		lp.getEmail().sendKeys(uname);
+		lp.getPass().sendKeys(pass);
+		System.out.println(status);
 		lp.getLogin().click();
 	}
 
+	
+	@DataProvider
+	public Object[][] getData() {
+		Object [][] data = new Object[2][3];
+		data[0][0] = "nonres@qw.com";
+		data[0][1] = "1233!";
+		data[0][2] = "non restricted";
+		
+		data[1][0] = "res@qw.com";
+		data[1][1] = "1234!";
+		data[1][2] = "restricted";
+		
+		return data;
+	}
 }
