81bbea5cc37ff53a7e0d5d50a9f2aa1c59b50b41
Ala123Sobhan <ala123sobhan@gmail.com>
2023-07-29 17:38:11 -0500

    webdriver

--- src/main/java/resources/base.java
+++ src/main/java/resources/base.java
@@ -12,12 +12,15 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class base {
 
@@ -35,28 +38,50 @@ public class base {
 		// String browserName = prop.getProperty("browser");
 		String browserName = System.getProperty("browser");
 
+//		if (browserName.contains("chrome")) {
+//
+//			System.setProperty("webdriver.chrome.driver",
+//					System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
+//			ChromeOptions option = new ChromeOptions();
+//			if (browserName.contains("headless")) {
+//				option.addArguments("--headless");
+//			}
+//			driver = new ChromeDriver(option);
+//		} else if (browserName.equals("firefox")) {
+//			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "\\driver\\geckodriver.exe");
+//			driver = new FirefoxDriver();
+//		} else if (browserName.equals("IE")) {
+//			System.setProperty("webdriver.ie.driver", System.getProperty("user.dir") + "\\driver\\IEDriverServer.exe");
+//			driver = new InternetExplorerDriver();
+//		}
+		
 		if (browserName.contains("chrome")) {
-
-			System.setProperty("webdriver.chrome.driver",
-					System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
-			ChromeOptions option = new ChromeOptions();
+			WebDriverManager.chromedriver().setup();
+			System.setProperty("webdriver.chrome.silentOutput", "true");
+		   ChromeOptions options = new ChromeOptions();
+		   options.setPageLoadStrategy(PageLoadStrategy.EAGER);
+			// options.addArguments("--incognito");
+			// driver = new ChromeDriver(options);
+		 
 			if (browserName.contains("headless")) {
-				option.addArguments("--headless");
+				//ChromeOptions options = new ChromeOptions();
+				options.addArguments("--headless");
+				options.addArguments("--disable-gpu");
+				options.addArguments("--no-sandbox");
+				options.addArguments("--allow-insecure-localhost");
+				options.addArguments("--disable-extensions");
+				options.addArguments("--disable-dev-shm-usage");
+				options.addArguments("--window-size=1400,800");
+				driver = new ChromeDriver(options);
+			} else {
+				driver = new ChromeDriver(options);
 			}
-			driver = new ChromeDriver(option);
-		} else if (browserName.equals("firefox")) {
-			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "\\driver\\geckodriver.exe");
-			driver = new FirefoxDriver();
-		} else if (browserName.equals("IE")) {
-			System.setProperty("webdriver.ie.driver", System.getProperty("user.dir") + "\\driver\\IEDriverServer.exe");
-			driver = new InternetExplorerDriver();
-		}
 
 		driver.manage().timeouts().implicitlyWait(12, TimeUnit.SECONDS);
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		return driver;
-
+		}
 	}
 
 	public String [] getScreenshot(String testcaseName, WebDriver driver) throws IOException {
